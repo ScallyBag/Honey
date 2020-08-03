ea8cfdee337ce0d6225f3b6a8ea5c75b5da9eeea
MichaelB7 <michaelbyrne1978@gmail.com>
2020-08-02 20:58:36 -0400

    add changes now , to work on master

--- src/build.sh
+++ src/build.sh
@@ -32,7 +32,7 @@ BUILD="profile-build"
 
 #make function
 function mke() {
-CXXFLAGS='-flto -mbmi ' make -j30 $BUILD $ARCH $COMP "$@"
+CXXFLAGS=' -mbmi ' make -j30 $BUILD $ARCH $COMP "$@"
 }
 
 mke WEAK=yes && wait
