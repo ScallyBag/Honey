36f3f3b269d00ec0e45e6edc73b311fab0d5a379
MichaelB7 <michaelbyrne1978@gmail.com>
2020-08-12 23:50:17 -0400

    spelling corrections

--- src/getnet.sh
+++ src/getnet.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
+#name if "getnet.sh" or something you prefer
 # by MichaelB7  08/12/20
 # A little script to update to current SF net and to rename net file to "eval.bin"
-# designed to be run from thr src folder , modify as you wish.
+# designed to be run from the src folder , modify as you wish.
 # This script is covered by the usual disclaimers, meaning that I am not possibly
-# responsible, finanacially or otherwise, for anything that may possibly go
-# wrong. There is absolutley no implied warranty, merchantability
-# or fitness for any particular purpose whatsover.
+# responsible, financially or otherwise, for anything that may possibly go
+# wrong. There is absolutely no implied warranty, merchantability
+# or fitness for any particular purpose whatsoever.
 
 
 cd ../  ## move up a directory, modify to your own preference
@@ -17,9 +19,9 @@ binurl=" https://tests.stockfishchess.org/api/nn/$sha256nn"
 ##renames nnue file to eval.bin , modify "eval.bin" to your own preference
 wgetnet="wget -O eval.bin"
 
-## pulls the most recent and latest officical NN from the SF Github repository
+## pulls the most recent and latest official NN from the SF GitHub repository
 $wgetnet $binurl
-rm ucioption.cpp   ## no lonnger needed.
+rm ucioption.cpp   ## no longer needed.
 sleep 1  ##  need to wait 1 second, before moving file or might error as being busy
 
 # I like to keep it in my SF build folder, modify to your own preference
