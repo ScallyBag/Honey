fa18c6a925f2bc730a294bedd526d0151ff90049
MichaelB7 <michaelbyrne1978@gmail.com>
2021-03-27 00:26:52 -0400

    Futility pruning tweak, take 2
    Bench: 5102945

--- src/search.cpp
+++ src/search.cpp
@@ -66,7 +66,7 @@ namespace {
 
   // Futility margin
   Value futility_margin(Depth d, bool improving) {
-    return Value(234 * (d - improving));
+    return Value(256 * (d - improving));
   }
 
   // Reductions lookup table, initialized at startup
