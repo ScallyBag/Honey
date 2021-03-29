135b6e49941d65f8a3daa7bd87334470f3ae1465
FauziAkram <fauzi.dabat@hotmail.com>
2021-03-29 16:06:47 +0300

    Update evaluate.cpp

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1091,8 +1091,8 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&]()
       {
-         int material = pos.non_pawn_material() + 2 * PawnValueMg * pos.count<PAWN>();
-         int scale =  641
+         int mat = pos.NN_material();
+         int scale =  646
                     + material / 32
                     - 4 * pos.rule50_count();
 
