f13f9074558e4c613d05c7fb64b75bcc49083f6e
MichaelB7 <michaelbyrne1978@gmail.com>
2021-03-31 12:03:27 -0400

    Take 4
    Bench: 4983862

--- src/search.cpp
+++ src/search.cpp
@@ -1167,17 +1167,16 @@ moves_loop: // When in check, search starts from here
       }
 
       // Check extension (~2 Elo)
-      else if ( PvNode )
+      else if ( !cutNode || PvNode )
       {
         if (    givesCheck
                && (pos.is_discovered_check_on_king(~us, move) || pos.see_ge(move)))
           extension = 1;
-      }
       // Last captures extension
-      else if (   PieceValue[EG][pos.captured_piece()] > PawnValueEg
+      else if ( PieceValue[EG][pos.captured_piece()] > PawnValueEg
                && pos.non_pawn_material() <= 2 * RookValueMg)
           extension = 1;
-
+        }
       // Add extension to new depth
       newDepth += extension;
 
