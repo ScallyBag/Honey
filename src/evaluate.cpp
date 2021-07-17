98ef30b673ad24d0657b711af6555b49b22f0d54
MichaelB7 <michaelbyrne1978@gmail.com>
2021-07-16 23:00:28 -0400

    Simplify adjusted_nnue.
    Bench: 5102558

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1090,11 +1090,7 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&]()
       {
-         int scale =   903
-                     + 32 * pos.count<PAWN>()
-                     + 32 * pos.non_pawn_material() / 1024;
-
-         Value nnue = NNUE::evaluate(pos, true) * scale / 1024;
+         Value nnue = NNUE::evaluate(pos, true);
 
          if (pos.is_chess960())
              nnue += fix_FRC(pos);
